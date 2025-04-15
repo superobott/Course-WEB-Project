63af0b116c3c9eaad8c0ff22ae176bbd8f4df939
superobott <nikitke@gmail.com>
2025-04-16 00:47:38 +0300

    push

--- HistoryFlow/src/logic/index.js
+++ HistoryFlow/src/logic/index.js
@@ -113,3 +113,16 @@ document.addEventListener('DOMContentLoaded', function() {
     // Uncomment to enable automatic redirect if user is already logged in
     // checkLoggedInStatus();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginButton = document.querySelector(".login-button");
+  const registerButton = document.querySelector(".register-button");
+
+  loginButton.addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
+
+  registerButton.addEventListener("click", () => {
+    window.location.href = "register.html";
+  });
+});
