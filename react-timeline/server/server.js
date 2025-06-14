014456b46f3655cd6e9a5ba55d2803f7c425bcf4
superobott <nikitke@gmail.com>
2025-06-15 01:44:47 +0300

    commit

--- react-timeline/server/server.js
+++ react-timeline/server/server.js
@@ -16,15 +16,22 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(express.text({ type: '*/*' }));
 
-// Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://Team_20:Team_20@web-database.y3fu8pk.mongodb.net/Timeline?retryWrites=true&w=majority&appName=Web-DataBase')
+// Connect to MongoDB Atlas with options
+mongoose.connect('mongodb+srv://Team_20:Team_20@web-database.y3fu8pk.mongodb.net/Timeline?retryWrites=true&w=majority&appName=Web-DataBase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+})
   .then(() => {
     console.log('MongoDB connected successfully');
+    console.log('Connection URL:', mongoose.connection.client.s.url);
     // Debug: Check if we can access the searches collection
     mongoose.connection.db.collection('searches').countDocuments()
       .then(count => {
         console.log(`Number of documents in searches collection: ${count}`);
-      });
+      })
+      .catch(err => console.error('Error accessing searches collection:', err));
   })
   .catch(err => console.error('MongoDB connection error:', err));
 
